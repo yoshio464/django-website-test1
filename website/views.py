06dada50c6677fb7b879cc4a955e97c48233a629
kusama <dayecaojian@gmail.com>
2021-04-20 12:02:01 +0900

    first commit

---
+++ website/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['username'] = '太郎'
+        return context
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['totals'] = 1234556
+        context['skills'] = [
+            'Python',
+            'Javascript',
+            'HTML, CSS'
+        ]
+        return context
+
